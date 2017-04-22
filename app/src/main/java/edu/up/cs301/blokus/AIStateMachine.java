9a63b33571b633c544b131a23ab36b65d9e54c02
Lucy Davidson <davidsol20@up.edu>
2017-04-22 13:32:31 -0700

    simpleAI transferred to new class

--- app/src/main/java/edu/up/cs301/blokus/AIStateMachine.java
+++ app/src/main/java/edu/up/cs301/blokus/AIStateMachine.java
@@ -15,91 +15,14 @@ import edu.up.cs301.game.GameComputerPlayer;
  */
 public class AIStateMachine {
 
-    interface AIState {
-        AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state);
-    }
 
-    static Random r;
+
+
 
     public AIStateMachine()
     {
-        r = new Random();
+
     }
 
-    enum AIStates implements AIState {
-        SelectPiece {
-            @Override
-            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
-            {
-                int[][] pieces = state.getPlayerPieces();
-                int pieceIndex, pieceID;
-                //find piece to play
-                do {
-                    //gets random number between 0 & 20
-                    pieceIndex = 0;//r.nextInt(21);
-                    pieceID = pieces[state.getPlayerTurn()][pieceIndex];
-                } while (pieceID == -1);
 
-                SelectPieceTemplateAction spta =
-                        new SelectPieceTemplateAction(AI, pieceID);
-
-                AI.setAction(spta);
-                return SelectBoardBlok;
-            }
-        },
-        SelectBoardBlok {
-            @Override
-            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
-            {
-                ArrayList<Blok> cpValidMoves = state.getValidCorners(state.getPlayerTurn());
-
-                Blok selectedBoardBlok = cpValidMoves.get(r.nextInt(cpValidMoves.size()));
-
-                SelectValidBlokOnBoardAction svboba =
-                        new SelectValidBlokOnBoardAction(AI, selectedBoardBlok);
-
-                AI.setAction(svboba);
-                return SelectPieceBlok;
-            }
-        },
-        SelectPieceBlok {
-            @Override
-            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
-            {
-                PieceTemplate selectedPiece = state.getSelectedPiece();
-
-                PieceBlok[] bloksOnPiece = selectedPiece.getPieceShape();
-
-                int selectedBlokID = 0;
-
-                SelectBlokOnSelectedPieceAction sbospa =
-                        new SelectBlokOnSelectedPieceAction(AI, selectedBlokID);
-
-                AI.setAction(sbospa);
-                return ConfirmPlacement;
-            }
-        },
-        Rotate {
-            @Override
-            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
-            {
-                return ConfirmPlacement;
-            }
-        },
-        Flip {
-            @Override
-            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
-            {
-                return ConfirmPlacement;
-            }
-        },
-        ConfirmPlacement {
-            @Override
-            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
-            {
-                AI.setAction(new ConfirmPiecePlacementAction(AI));
-                return SelectPiece;
-            }
-        }
-    }
 }
--- app/src/main/java/edu/up/cs301/blokus/BlokusTestAIPlayer.java
+++ app/src/main/java/edu/up/cs301/blokus/BlokusTestAIPlayer.java
@@ -1,6 +1,16 @@
 package edu.up.cs301.blokus;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import edu.up.cs301.blokus.actions.ConfirmPiecePlacementAction;
 import edu.up.cs301.blokus.actions.DoNothingAction;
+import edu.up.cs301.blokus.actions.FlipSelectedPieceAction;
+import edu.up.cs301.blokus.actions.RotateSelectedPieceAction;
+import edu.up.cs301.blokus.actions.SelectBlokOnSelectedPieceAction;
+import edu.up.cs301.blokus.actions.SelectPieceTemplateAction;
+import edu.up.cs301.blokus.actions.SelectValidBlokOnBoardAction;
+import edu.up.cs301.blokus.pieces.PieceTemplate;
 import edu.up.cs301.game.GameComputerPlayer;
 import edu.up.cs301.game.actionMsg.GameAction;
 import edu.up.cs301.game.infoMsg.GameInfo;
@@ -10,10 +20,18 @@ import edu.up.cs301.game.infoMsg.GameInfo;
  */
 public class BlokusTestAIPlayer extends GameComputerPlayer {
 
-    BlokusGameState gameState;
-    AIStateMachine stateMachine;
-    GameAction curAction;
-    AIStateMachine.AIState curState;
+    interface AIState {
+        AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state);
+    }
+
+    private BlokusGameState gameState;
+    private AIStateMachine stateMachine;
+    private GameAction curAction;
+    private AIState curState;
+    public int rotateTracker;
+    public int flipTracker;
+
+    static Random r;
 
     /**
      * constructor
@@ -24,7 +42,8 @@ public class BlokusTestAIPlayer extends GameComputerPlayer {
         super(name);
 
         this.stateMachine = new AIStateMachine();
-        curState = AIStateMachine.AIStates.SelectPiece;
+        curState = AIStates.SelectPiece;
+        r = new Random();
     }
 
     @Override
@@ -45,6 +64,134 @@ public class BlokusTestAIPlayer extends GameComputerPlayer {
                 periodic();
 
                 game.sendAction(curAction);
+            }
+        }
+    }
+
+    enum AIStates implements AIState {
+        SelectPiece {
+            @Override
+            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
+            {
+                int[][] pieces = state.getPlayerPieces();
+                int pieceIndex, pieceID;
+                //find piece to play
+                do {
+                    //gets random number between 0 & 20
+                    pieceIndex = r.nextInt(21);
+                    pieceID = pieces[state.getPlayerTurn()][pieceIndex];
+                } while (pieceID == -1);
+
+                SelectPieceTemplateAction spta =
+                        new SelectPieceTemplateAction(AI, pieceID);
+
+                AI.setAction(spta);
+                return SelectPieceBlok;
+            }
+        },
+        SelectBoardBlok {
+            @Override
+            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
+            {
+                //gets arrayList of valid moves
+                ArrayList<Blok> cpValidMoves = state.getValidCorners(state.getPlayerTurn());
+
+                Blok selectedBoardBlok = cpValidMoves.get(r.nextInt(cpValidMoves.size()));
+
+                SelectValidBlokOnBoardAction svboba =
+                        new SelectValidBlokOnBoardAction(AI, selectedBoardBlok);
+
+                AI.setAction(svboba);
+                return ConfirmPlacement;
+            }
+        },
+        SelectPieceBlok {
+            @Override
+            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
+            {
+                PieceTemplate selectedPiece = state.getSelectedPiece();
+
+                PieceBlok[] bloksOnPiece = selectedPiece.getPieceShape();
+
+                int selectedBlokID = r.nextInt(bloksOnPiece.length);
+
+                SelectBlokOnSelectedPieceAction sbospa =
+                        new SelectBlokOnSelectedPieceAction(AI, selectedBlokID);
+
+                AI.setAction(sbospa);
+                return SelectBoardBlok;
+            }
+        },
+        Rotate {
+            @Override
+            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
+            {
+                AIState tempState = ConfirmPlacement;
+                GameAction tempAction = new DoNothingAction(AI, false);
+                switch (AI.rotateTracker)
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        tempAction = new RotateSelectedPieceAction(AI);
+                        tempState = ConfirmPlacement;
+                        break;
+                    case 4:
+                        tempAction = new FlipSelectedPieceAction(AI);
+                        tempState = ConfirmPlacement;
+                        break;
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                        tempAction = new RotateSelectedPieceAction(AI);
+                        tempState = ConfirmPlacement;
+                        break;
+                }
+
+                AI.rotateTracker++;
+
+                if (AI.rotateTracker == 9)
+                {
+                    AI.rotateTracker = 0;
+                    tempState = SelectPiece;
+                }
+
+
+                AI.setAction(tempAction);
+                return tempState;
+            }
+        },
+        Flip {
+            @Override
+            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
+            {
+                return ConfirmPlacement;
+            }
+        },
+        ConfirmPlacement {
+            @Override
+            public AIState checkState(BlokusTestAIPlayer AI, BlokusGameState state)
+            {
+                Blok selectedBoardBlok = state.getSelectedBoardBlok();
+                PieceTemplate selectedPiece = state.getSelectedPiece();
+                int selectedPieceBlokId = state.getSelectedPieceBlokId();
+                Blok[][] board = state.getBoardState();
+
+                if (state.prepareValidMove(selectedBoardBlok,
+                        selectedPiece,
+                        selectedPieceBlokId,
+                        board) == null)
+                {
+                    AI.setAction(new DoNothingAction(AI, false));
+                    return Rotate;
+                }
+                else
+                {
+                    AI.setAction(new ConfirmPiecePlacementAction(AI));
+                    return SelectPiece;
+                }
             }
         }
     }
